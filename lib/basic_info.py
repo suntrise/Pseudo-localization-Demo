b5c20f2c303e8ae33553c5f447c04bc821b4872e
suntrise <89229642+suntrise@users.noreply.github.com>
2023-08-11 13:41:27 +0800

    Update v4.51p

--- lib/basic_info.py
+++ lib/basic_info.py
@@ -4,7 +4,7 @@ from lib import log # 日志输出库
 log.out(0, "Basic Information ready...")
 
 # 基本信息
-ver = "v4.5.1p"
+ver = "v4.51p"
 author = "Suntrise (STR)"
 auth_abbr = "STR"
 title = "伪本地化演示程序 " + ver + " by " + auth_abbr
